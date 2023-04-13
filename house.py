23b8c01f620cb29f2fe125fcbbfbc4b7cb4382ef
Netoamf <tonemfn@gmail.com>
2023-04-12 23:33:57 -0300

    Lecture 1 - conditionals

---
+++ house.py
@@ -0,0 +1,9 @@
+name = input("whats your name? ")
+
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("who?")
